725750ea2ff969f285b7f05d8ce96f17badc25b2
Ankit Arora <ankit992@live.in>
2017-10-15 15:47:26 +0530

    added firebase

--- manager/src/app.js
+++ manager/src/app.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDFYmRf5-NKDxnR5aSqUFjJrwXuJThBa9s',
+            authDomain: 'manager-84b75.firebaseapp.com',
+            databaseURL: 'https://manager-84b75.firebaseio.com',
+            projectId: 'manager-84b75',
+            storageBucket: '',
+            messagingSenderId: '305560767249'
+          };
+          firebase.initializeApp(config);
+    }
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
                         Hello!
