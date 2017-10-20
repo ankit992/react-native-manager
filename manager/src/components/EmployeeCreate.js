714e9b0f22b16165e25bca0c72430e1024134699
Ankit Arora <ankit992@live.in>
2017-10-21 00:25:43 +0530

    working on employee create form continue

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text> Employee Form </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input 
+                        label='Name'
+                        placeholder='Jane'
+                        value={this.props.name}
+                        onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input 
+                        label='Phone'
+                        placeholder='555-555-5555'
+                        phone={this.props.phone}
+                        onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+                    />
+                </CardSection>
+
+                <CardSection>
+
+                </CardSection>
+
+                <CardSection>
+                    <Button>Create</Button>
+                </CardSection>
+                
+            </Card>
         );
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+
+    return { name, phone, shift };
+
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
